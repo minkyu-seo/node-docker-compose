687b5c98400b331252e226ee5906d0c8658d1b75
seominkyu <sym221@cashwalk.io>
2020-03-30 10:58:55 +0900

    1. ts,eslint 설정 추가.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import graphqlHTTP from "express-graphql";
-import { buildSchema } from "graphql";
-import { Request, Response } from "express";
-import * as Amqp from "amqp-ts";
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+// eslint-disable-next-line no-unused-vars
+import express from 'express';
+
+// import { Request, Response } from 'express';
 
 const schema = buildSchema(`
   type Query {
@@ -11,31 +12,35 @@ const schema = buildSchema(`
 `);
 
 const root = {
-    hello: () => {
-        return 'Hello world!';
-    },
+  hello: () => 'Hello world!',
 };
 
 class App {
     public application : express.Application;
-    constructor(){
-        this.application = express();
+
+    constructor() {
+      this.application = express();
     }
 }
 
 const app = new App().application;
 
 app.use(express.json());
-app.use(express.urlencoded( {extended : false } ));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/",(req : Request , res : Response) => {
-    res.send("start");
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('start');
+});
+
+app.get('/js', (req: express.Request, res: express.Response) => {
+  res.sendFile('/source/import.js', { root: __dirname });
+  // res.send("start");
 });
 
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+  schema,
+  rootValue: root,
+  graphiql: true,
 }));
 
 interface Err extends Error {
@@ -44,44 +49,21 @@ interface Err extends Error {
 }
 
 // catch 404 and forward to error handler
-app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    let err = new Error('Not Found') as Err
-    err.status = 404
-    next(err)
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const err = new Error('Not Found') as Err;
+  err.status = 404;
+  next(err);
 });
 
 // error handle
-app.use(function (err: Err, req: express.Request, res: express.Response, next: express.NextFunction) {
-    // render the error page
-    res.status(err.status || 500)
-    res.json({
-        message: err.message,
-        data: err.data
-    })
-});
-
-const connection = new Amqp.Connection("amqp://localhost");
-const exchange = connection.declareExchange("ExchangeName");
-const queue = connection.declareQueue("QueueName");
-queue.bind(exchange);
-queue.activateConsumer((message) => {
-    console.log("Message received: " + message.getContent());
-});
-
-// it is possible that the following message is not received because
-// it can be sent before the queue, binding or consumer exist
-// const msg = new Amqp.Message("Test");
-// exchange.send(msg);
-
-connection.completeConfiguration().then(() => {
-    // the following message will be received because
-    // everything you defined earlier for this connection now exists
-    var msg2 = new Amqp.Message("Test2");
-    exchange.send(msg2);
+app.use((err: Err, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    data: err.data,
+  });
 });
 
 const port = 4000;
-app.listen(port,() => console.log(`Server Start : ${port} `));
-
-
-
+app.listen(port, () => console.log(`Server Start : ${port} `));
